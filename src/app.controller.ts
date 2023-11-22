a6c242325297046a4d1b6f0659e2ee01f0e2934c
Tadesse Alemayehu <tadessedev@gmail.com>
2023-11-22 16:18:25 +0300

    Subscribe to add numbers event and emit a multiplay message

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { EventPattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @EventPattern('add')
+  sumTwoNumbers(data: {num1: number, num2: number}) {
+    return this.appService.sumTwoNumbers(data);
+  }
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
+  constructor(@Inject('MATH_SERVICE') private readonly client: ClientProxy) {}
+  sumTwoNumbers(data: { num1: number; num2: number }) {
+    console.log('summing up numbers');
+    let result = Object.values(data).reduce((a, b) => a + b, 0);
+    console.log(`result is ${result} \npropagating to auth service`);
+     this.client.send<number>('multiply', [result, 12]).subscribe((ret) => {
+      console.log('getting the result from auth',ret)
+     });
+    return result;
+  }
   getHello(): string {
     return 'Hello World!';
   }
